385cf4cdc86ae559b9b92c8b5b5912daa160b169
SMTR <mario@smtr.dev>
2026-02-25 15:03:12 +0700

    auto-backup: 2026-02-25 23:45

---
+++ invest-sns/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/invest-sns',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
